dd8ac204b05b403c649114ad02238ddffb9c949f
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-02-13 12:18:44 -0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.create(name: "Harry Potter and the Sorcerer's Stone", image: "https://upload.wikimedia.org/wikipedia/en/b/bf/Harry_Potter_and_the_Sorcerer's_Stone.jpg", author: "JK Rowling")
+Book.create(name: "Osama Bin Laden", image: "https://images-na.ssl-images-amazon.com/images/I/51tA0jfmN%2BL._SX334_BO1,204,203,200_.jpg", author: "Michael Scheuer")
+Book.create(name: "The Hobbit", image: "https://images-na.ssl-images-amazon.com/images/I/41aQPTCmeVL._SY344_BO1,204,203,200_.jpg", author: "J.R.R Tolkein")
+Book.create(name: "Saving Capitalism", image: "http://argonautnews.com/wp-content/uploads/2015/10/Reich-Book-Cover.jpg", author: "Robert Reich")
+Book.create( name: "Starting Strength", image: "https://images-na.ssl-images-amazon.com/images/I/91l1%2BK3sK1L.jpg", author: "Mark Rippetoe")
